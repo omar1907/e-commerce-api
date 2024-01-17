4d1209d780b8c4530aedb7fd81f00771a343105b
omar <omarmah1907@gmail.com>
2024-01-17 11:03:58 +0200

    deploy commit

--- app.js
+++ app.js
@@ -40,21 +40,12 @@ app.use(xss())
 app.use(mongoSanitize())
 
 
-app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 
 app.use(express.static('./public'))
 app.use(fileUpload())
 
-app.get('/',(req,res)=>{
-    // console.log(req.cookies);
-    res.send('E-commerce Project')
-})
-app.get('/api/v1',(req,res)=>{
-    console.log(req.signedCookies);
-    res.send('E-commerce')
-})
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/users',userRouter)
